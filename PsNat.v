d46e0516620f28f95a073e645c5fa47c1b724550
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-10-04 17:02:19 +0900

    simplify

--- PsNat.v
+++ PsNat.v
@@ -35,37 +35,6 @@ Notation eval_instnat := (fun n => proj2_sig (exists_instnat n)).
 Hint Resolve (eval_instnat : forall n, instnat_spec n (instnat n)).
 
 (*
-instnat_eqmap:
-  任意の自然数 n, m と任意の命令 i について、i が自然数 n, m としての仕様を同時
-  に満たしていれば、必ず n = m である。
-  よって、n /= m であればある命令が自然数 n, m としての仕様を同時に満たすことは
-*)
-Lemma instnat_eqmap :
-  forall n m i, instnat_spec n i -> instnat_spec m i -> n = m.
-Proof.
-  move=> n m i1 H0 H1.
-  have H2: (([], replicate n instpop) |=>* ([], replicate m instpop) \/
-      ([], replicate m instpop) |=>* ([], replicate n instpop)).
-    apply (eval_semi_uniqueness ([instpop], [i1 ; instexec])).
-    - evalpartial H0.
-      evalauto.
-      evalpartial evalseq_replicate.
-      rtcrefl.
-      apply app_nil_r.
-    - evalpartial H1.
-      evalauto.
-      evalpartial evalseq_replicate.
-      rtcrefl.
-      apply app_nil_r.
-  have: (replicate n instpop = replicate m instpop).
-    by destruct H2 ; destruct n ; destruct m ; inversion H ;
-      (inversion H2 || simpl ; f_equal).
-  clear.
-  move: m ; induction n ; intro ; destruct m ; simpl ; intros ;
-    (congruence || f_equal ; apply IHn ; congruence).
-Qed.
-
-(*
 instnat_repeat:
   繰り返しの命令。
 *)
@@ -83,6 +52,33 @@ Defined.
 
 Notation instnat_repeat := (proj1_sig exists_instnat_repeat).
 Notation eval_instnat_repeat := (proj2_sig exists_instnat_repeat).
+
+(*
+instnat_eqmap:
+  任意の自然数 n, m と任意の命令 i について、i が自然数 n, m としての仕様を同時
+  に満たしていれば、必ず n = m である。
+  よって、n /= m であればある命令が自然数 n, m としての仕様を同時に満たすことは
+*)
+Lemma instnat_eqmap :
+  forall n m i, instnat_spec n i -> instnat_spec m i -> n = m.
+Proof.
+  move=> n m i1 H0 H1.
+  have H2: (([], replicate n instpop) |=>* ([], replicate m instpop) \/
+      ([], replicate m instpop) |=>* ([], replicate n instpop)).
+    apply (eval_semi_uniqueness ([instpop], [i1 ; instexec])).
+    - evalpartial (eval_instnat_repeat n).
+      rtcrefl.
+      apply app_nil_r.
+    - evalpartial (eval_instnat_repeat m).
+      rtcrefl.
+      apply app_nil_r.
+  have: (replicate n instpop = replicate m instpop).
+    by destruct H2 ; destruct n ; destruct m ; inversion H ;
+      (inversion H2 || simpl ; f_equal).
+  clear.
+  move: m ; induction n ; intro ; destruct m ; simpl ; intros ;
+    (congruence || f_equal ; apply IHn ; congruence).
+Qed.
 
 (*
 instnat_succ:
