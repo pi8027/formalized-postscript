edfae5ac3f665ce8f36233d6503e7462d2d54865
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-10-12 05:13:59 +0900

    fix

--- PsExample.v
+++ PsExample.v
@@ -2,5 +2,5 @@ Require Import
   Strings.String ssreflect Common PsCore PsTemplate PsBool PsNat.
 
 Eval compute in (inst_to_pscode (proj1_sig (exists_instnat_sub))).
-Eval compute in (inst_to_pscode (proj1_sig (exists_inst_fill_template 1
+Eval compute in (inst_to_pscode (proj1_sig (exists_inst_fill_template 8
   (insttpair (insttpush (insttpush (instthole 2))) (instthole 4))))).
