24c6c4e6cf2a4248948185de850308ed53415908
Kazuhiko Sakaguchi <pi8027@gmail.com>
2013-02-26 06:00:03 +0900

    simplify

--- stdlib_ext.v
+++ stdlib_ext.v
@@ -5,24 +5,14 @@ Require Import
 
 Set Implicit Arguments.
 
-Lemma nseq_app : forall A n m (a : A), nseq n a ++ nseq m a = nseq (n + m) a.
-Proof.
-  move => A; elim => //=.
-  by move => n IH m a; f_equal.
-Qed.
-
 Lemma nseq_rev_id : forall A n (a : A), nseq n a = rev (nseq n a).
 Proof.
   move => A n a.
-  rewrite -{1}(cats0 (nseq n a)) /rev -[[::]]/(nseq 0 a).
-  move: n (0); elim => //= n IH m.
-  by rewrite -(IH m.+1) !nseq_app addnS.
+  rewrite /rev -{1}(cats0 (nseq n a)) -[[::]]/(nseq 0 a).
+  move: n 0; elim => //= n IH m; rewrite -(IH m.+1); clear.
+  by elim: n => //= n IH; f_equal.
 Qed.
 
-(*
-rt1n_trans':
-  clos_refl_trans_1n は推移関係である。
-*)
 Lemma rt1n_trans' : forall A (R : relation A) (x y z : A),
   clos_refl_trans_1n A R x y -> clos_refl_trans_1n A R y z ->
   clos_refl_trans_1n A R x z.
@@ -37,19 +27,14 @@ Definition ltof A (f : A -> nat) (a b : A) := (f a < f b)%nat.
 Theorem well_founded_ltof :
   forall (A : Type) (f : A -> nat), well_founded (ltof f).
 Proof.
-  move => A f x.
+  rewrite /ltof => A f x.
   move: {2}(f x) (leqnn (f x)) => n.
   move: n x.
-  elim.
-  - move => x.
-    rewrite (leqn0 (f x)).
-    case/eqP => H.
-    constructor; rewrite /ltof H => y.
-    rewrite ltn0 => H0.
-    case: (not_false_is_true H0).
-  - move => n IHn x H.
-    constructor; rewrite /ltof => y H0.
-    apply IHn.
+  elim => [ | n IHn ] x H; constructor => y H0.
+  - apply False_ind, notF.
+    rewrite -(ltn0 (f y)).
+    apply (leq_trans H0 H).
+  - apply IHn.
     rewrite -ltnS.
     apply (leq_trans H0 H).
 Defined.
